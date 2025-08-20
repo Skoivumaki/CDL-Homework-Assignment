4a4eb714cd1dccc7b15dc07b6cc5675527ef39dc
Skoivumaki <sakari.koivumaki@gmail.com>
2025-08-20 03:29:11 +0300

    add: major design overhaul with tailwind with minor logic changes

--- movie-review-app/src/components/ViewDetails.vue
+++ movie-review-app/src/components/ViewDetails.vue
@@ -3,37 +3,13 @@ import { useFetch } from '@/composables/useFetch'
 import { computed, ref } from 'vue'
 import ViewReviews from './ViewReviews.vue'
 import SubmitReview from './SubmitReview.vue'
-
-// Todo: Move types to own file
-interface Movie {
-  Title: string
-  Year: string
-  Rated?: string
-  Released?: string
-  Runtime?: string
-  Genre?: string
-  Director?: string
-  Writer?: string
-  Actors?: string
-  Plot?: string
-  Language?: string
-  Country?: string
-  Awards?: string
-  Poster?: string
-  Metascore?: string
-  imdbRating?: string
-  imdbVotes?: string
-  Type?: string
-  DVD?: string
-  BoxOffice?: string
-  Production?: string
-  Website?: string
-  Response?: string
-}
+import { vOnClickOutside } from '@vueuse/components'
+import type { Movie } from '@/types/movie'
 
 const props = defineProps<{ imdbID: string }>()
 const emit  = defineEmits(['close'])
 const selectedMovieID = ref('')
+const isViewVisible = ref(false);
 
 const { data: movieData, loading, error } = useFetch<Movie>(
   computed(() => props.imdbID ? `/api/movie/${props.imdbID}` : '')
@@ -45,24 +21,32 @@ function onBack() {
 
 function openReviews(m: string) {
   selectedMovieID.value = m
+  isViewVisible.value = true;
+}
+
+function closeReviews() {
+  isViewVisible.value = false;
+  setTimeout(() => {
+    selectedMovieID.value = ''
+  }, 500);
 }
 
 </script>
 
 <template>
-  <div class="transform transition-all w-full h-full bg-primary p-0">
+  <div class="flex flex-col w-full h-full bg-primary">
     <button
-      class="bg-highlight border-solid p-2 text-white rounded-br-lg text-lg cursor-pointer relative transition duration-200 hover:bg-secondary"
+      class="bg-highlight border-solid p-2 w-30 text-primary rounded-br-lg text-lg cursor-pointer relative transition duration-200 hover:bg-secondary"
       @click="onBack"
     >
     Close
     </button>
-    <div class="p-2 flex-row">
+    <div class="p-2 flex-row h-fit flex-shrink-0">
       <div v-if="loading" class="text-highlight text-center">Loading…</div>
       <div v-else-if="error">{{ error }}</div>
       <template v-else-if="movieData">
-        <div>
-          <h1>{{ movieData.Title }} <small>({{ movieData.Year }})</small></h1>
+        <div class="max-h-80 overflow-hidden text-clip">
+          <h1 class="truncate">{{ movieData.Title }} <small>({{ movieData.Year }})</small></h1>
           <p><strong>Director:</strong> {{ movieData.Director }}</p>
           <p><strong>Genre:</strong> {{ movieData.Genre }}</p>
           <p><strong>Plot:</strong> {{ movieData.Plot }}</p>
@@ -73,21 +57,38 @@ function openReviews(m: string) {
           :imdbID=imdbID
           :Title=movieData.Title
         />
-        <button @click="openReviews(props.imdbID)"
-          style="cursor:pointer; display:flex; align-items:center;">
-          View reviews
-        </button>
       </template>
-      <div
-      v-if="selectedMovieID"
-      class="overlay"
-      @click.self="selectedMovieID = ''"
+    </div>
+    <div class="py-2 w-40 flex-shrink-0">
+      <button
+        v-if="!isViewVisible"
+        @click="openReviews(props.imdbID)"
+        class="bg-highlight p-2 text-primary rounded-br-lg text-lg hover:bg-secondary"
       >
-        <ViewReviews
-          :imdbID="selectedMovieID"
-          @hide="selectedMovieID = ''"
-        />
-      </div>
+        View Reviews
+      </button>
+      <button
+        v-if="isViewVisible"
+        @click="closeReviews"
+        class="bg-highlight p-2 text-primary rounded-br-lg text-lg hover:bg-secondary"
+      >
+        Hide Reviews
+      </button>
+    </div>
+    <div class="flex-1 overflow-hidden">
+      <Transition name="transition">
+        <div
+          v-if="isViewVisible"
+          v-on-click-outside="closeReviews"
+          class="px-2 w-full h-full overflow-y-auto overscroll-contain"
+        >
+          <ViewReviews
+            :imdbID="selectedMovieID"
+            @hide="closeReviews"
+            class="w-full"
+          />
+        </div>
+      </Transition>
     </div>
   </div>
 </template>
